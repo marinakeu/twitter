f9706c8d8ac2f4a1e978dcad1e291fa365b9ba04
marinakeu <marina_keunecke@hotmail.com>
2019-02-18 21:19:57 -0300

    1a versao ok - adicionando tweet

--- twitter.js
+++ twitter.js
@@ -1,3 +1,14 @@
-document.getElementById("myBtn").addEventListener("click", function(){
-  document.getElementById("demo").innerHTML = "Hello World";
-});
+let tweetButton = document.getElementById('tweet-button');
+console.log(tweetButton);
+
+function newTweet(){    
+    console.log('entrou');
+let tweetText = document.getElementById('tweet-area').value;
+console.log(tweetText);
+let paragraph = document.createElement('p');
+var message = document.createTextNode(tweetText);
+paragraph.appendChild(message);
+document.getElementById("tweets").appendChild(paragraph); 
+}
+
+tweetButton.addEventListener('click', newTweet);
