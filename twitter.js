ed5ea8883a5a2494459e9a3b3fa1a77c9f3a8ad4
marinakeu <marina_keunecke@hotmail.com>
2019-02-18 22:33:09 -0300

    2a versao ok - desativar botao e contar caracteres

--- twitter.js
+++ twitter.js
@@ -1,14 +1,28 @@
+let tweetArea = document.getElementById('tweet-area');
 let tweetButton = document.getElementById('tweet-button');
-console.log(tweetButton);
 
-function newTweet(){    
-    console.log('entrou');
-let tweetText = document.getElementById('tweet-area').value;
-console.log(tweetText);
-let paragraph = document.createElement('p');
-var message = document.createTextNode(tweetText);
-paragraph.appendChild(message);
-document.getElementById("tweets").appendChild(paragraph); 
+function showButton() {
+    let tweetLength = tweetArea.value.length;
+    if (tweetArea.value) {
+        tweetButton.style.visibility = 'visible';
+        let characters = 140 - tweetLength;
+        let counter = document.getElementById('counter');
+        counter.innerHTML = characters;
+
+    } else {
+        tweetButton.style.visibility = 'hidden';
+        counter.innerHTML = 140;
+    }
+}
+
+tweetArea.addEventListener('input', showButton);
+
+function newTweet() {
+    let tweetText = document.getElementById('tweet-area').value;
+    let paragraph = document.createElement('p');
+    let message = document.createTextNode(tweetText);
+    paragraph.appendChild(message);
+    document.getElementById("tweets").appendChild(paragraph);
 }
 
 tweetButton.addEventListener('click', newTweet);
