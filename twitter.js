5e70249f9aa15b6b8faf6c7df455082add1d0697
marinakeu <marina_keunecke@hotmail.com>
2019-02-19 22:01:57 -0300

    codigo reorganizado e reduzido

--- twitter.js
+++ twitter.js
@@ -1,17 +1,45 @@
 let tweetArea = document.getElementById('tweet-area');
 let tweetButton = document.getElementById('tweet-button');
+let counter = document.getElementById('counter');
 tweetButton.disabled = true;
 
-function showButton() {
+function btnEnable() {
+    tweetButton.disabled = false;
+    tweetButton.style.backgroundColor = '#1DA1F2';
+    tweetButton.style.cursor = 'pointer';
+}
+
+function btnDisabled() {
+    tweetButton.disabled = true;
+    tweetButton.style.backgroundColor = '#BFAFAC';
+    tweetButton.style.cursor = 'auto';
+}
+
+function reset() {
+    btnDisabled();
+    counter.innerHTML = 140;
+    counter.style.color = '#657786';
+    tweetArea.rows = 1;
+}
+
+function newRows() {
+    let cols = tweetArea.cols;
+    let arrayText = tweetArea.value.split('\n');
+    let rows = arrayText.length;
+    for (i = 0; i < arrayText.length; i++) {
+        rows += parseInt(arrayText[i].length / cols);
+    };
+    if (rows <= 1) tweetArea.rows = 1;
+    else tweetArea.rows = rows;
+}
+
+function typingTweet() {
     let tweetLength = tweetArea.value.length;
     let characters = 140 - tweetLength;
     if (tweetArea.value) {
-        let counter = document.getElementById('counter');
         counter.innerHTML = characters;
         if (characters >= 0) {
-            tweetButton.disabled = false;
-            tweetButton.style.backgroundColor = '#1DA1F2';
-            tweetButton.style.cursor = 'pointer';
+            btnEnable();
             if (characters < 20 && characters > 9) {
                 counter.style.color = 'orange';
             } else if (characters < 10) {
@@ -20,35 +48,18 @@ function showButton() {
                 counter.style.color = '#657786';
             }
         } else {
-            tweetButton.disabled = true;
-            tweetButton.style.backgroundColor = '#BFAFAC';
-            tweetButton.style.cursor = 'auto';
+            btnDisabled();
         }
     } else {
-        tweetButton.disabled = true;
-        tweetButton.style.backgroundColor = '#BFAFAC';
-        tweetButton.style.cursor = 'auto';
-        counter.innerHTML = 140;
-        counter.style.color = '#657786';
+        reset();
     }
-    
-    let cols = tweetArea.cols;
-    let arrayText = tweetArea.value.split('\n');
-    let rows = arrayText.length;
-    console.log(cols);
-    console.log(arrayText);
-    for (i = 0; i < arrayText.length; i++) {
-        rows += parseInt(arrayText[i].length / cols);
-    };
-    if (rows <= 1) tweetArea.rows = 1;
-    else tweetArea.rows = rows;
-    console.log(rows);
+    newRows();
 }
 
-tweetArea.addEventListener('input', showButton);
+tweetArea.addEventListener('input', typingTweet);
 
 function newTweet() {
-    let tweetText = tweetArea.value;    
+    let tweetText = tweetArea.value;
     let textParagraph = document.createElement('p');
     textParagraph.textContent = tweetText;
     let date = moment().format('HH:mm');
@@ -57,13 +68,9 @@ function newTweet() {
     let tweetBox = document.createElement('div');
     document.getElementById("tweets").appendChild(tweetBox);
     tweetBox.appendChild(textParagraph);
-    tweetBox.appendChild(dateParagraph);  
-    tweetArea.value = ''; 
-    tweetButton.disabled = true;
-    tweetButton.style.backgroundColor = '#BFAFAC';
-    tweetButton.style.cursor = 'auto';
-    counter.innerHTML = 140;
-    counter.style.color = '#657786';
+    tweetBox.appendChild(dateParagraph);
+    tweetArea.value = '';
+    reset();
 }
 
 tweetButton.addEventListener('click', newTweet);
