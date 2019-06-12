c6542aad9765a3c585341a33ce40256b1dc8071c
marinakeu <marina_keunecke@hotmail.com>
2019-06-12 13:07:56 -0300

    refatora - ajusta identação

--- twitter.js
+++ twitter.js
@@ -4,62 +4,62 @@ let counter = document.getElementById('counter');
 tweetButton.disabled = true;
 
 function btnEnable() {
-    tweetButton.disabled = false;
-    tweetButton.style.backgroundColor = '#1DA1F2';
-    tweetButton.style.cursor = 'pointer';
+  tweetButton.disabled = false;
+  tweetButton.style.backgroundColor = '#1DA1F2';
+  tweetButton.style.cursor = 'pointer';
 }
 
 function btnDisabled() {
-    tweetButton.disabled = true;
-    tweetButton.style.backgroundColor = '#BFAFAC';
-    tweetButton.style.cursor = 'auto';
+  tweetButton.disabled = true;
+  tweetButton.style.backgroundColor = '#BFAFAC';
+  tweetButton.style.cursor = 'auto';
 }
 
 function reset() {
-    btnDisabled();
-    tweetArea.value = '';
-    counter.innerHTML = 140;
-    counter.style.color = '#657786';
-    tweetArea.style.height = 'auto';
+  btnDisabled();
+  tweetArea.value = '';
+  counter.innerHTML = 140;
+  counter.style.color = '#657786';
+  tweetArea.style.height = 'auto';
 }
 
 function newRows() {
-    tweetArea.style.height = tweetArea.scrollHeight + 'px';
+  tweetArea.style.height = tweetArea.scrollHeight + 'px';
 }
 
 function typingTweet() {
-    let tweetLength = tweetArea.value.length;
-    let characters = 140 - tweetLength;
-    counter.innerHTML = characters;
-    if (tweetArea.value) {
-        if (characters >= 0) {
-            btnEnable();
-            if (characters < 20 && characters > 9) {
-                counter.style.color = 'orange';
-            } else if (characters < 10) {
-                counter.style.color = 'red';
-            } else if (characters > 19) {
-                counter.style.color = '#657786';
-            }
-        } else {
-            btnDisabled();
-        }
+  let tweetLength = tweetArea.value.length;
+  let characters = 140 - tweetLength;
+  counter.innerHTML = characters;
+  if (tweetArea.value) {
+    if (characters >= 0) {
+      btnEnable();
+      if (characters < 20 && characters > 9) {
+        counter.style.color = 'orange';
+      } else if (characters < 10) {
+        counter.style.color = 'red';
+      } else if (characters > 19) {
+        counter.style.color = '#657786';
+      }
     } else {
-        reset();
+      btnDisabled();
     }
-    newRows();
+  } else {
+    reset();
+  }
+  newRows();
 }
 
 tweetArea.addEventListener('input', typingTweet);
 
 function newTweet() {
-    let tweetText = tweetArea.value;
-    let date = moment().format('HH:mm');
-    let tweets = document.getElementById("tweets");
-    let tweetBox = document.createElement('div');
-    tweets.appendChild(tweetBox);
-    tweetBox.innerHTML = '<p>' + tweetText + '</p> <p>' + date + '</p>';
-    reset();
+  let tweetText = tweetArea.value;
+  let date = moment().format('HH:mm');
+  let tweets = document.getElementById("tweets");
+  let tweetBox = document.createElement('div');
+  tweets.appendChild(tweetBox);
+  tweetBox.innerHTML = '<p>' + tweetText + '</p> <p>' + date + '</p>';
+  reset();
 }
 
 tweetButton.addEventListener('click', newTweet);
